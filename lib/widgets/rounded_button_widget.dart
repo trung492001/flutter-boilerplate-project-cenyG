3d9ff2338238a2937139ed9ad9b2e8f946e50f67
DTTrung4901 <72913284+trung492001@users.noreply.github.com>
2023-03-26 13:07:57 +0700

    Initial commit

---
+++ lib/widgets/rounded_button_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class RoundedButtonWidget extends StatelessWidget {
+  final String buttonText;
+  final Color buttonColor;
+  final Color textColor;
+  final VoidCallback onPressed;
+
+  const RoundedButtonWidget({
+    Key? key,
+    required this.buttonText,
+    required this.buttonColor,
+    this.textColor = Colors.white,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: TextButton.styleFrom(
+        shape: StadiumBorder(),
+        backgroundColor: buttonColor,
+        padding: EdgeInsets.all(0),
+      ),
+      onPressed: onPressed,
+      child: Text(
+        buttonText,
+        style: Theme.of(context).textTheme.button!.copyWith(color: textColor),
+      ),
+    );
+  }
+}
